709b31065933eaa030b6a3f4a5d6b3efa9350e41
Kevin Lange <klange@dakko.us>
2015-04-28 09:56:30 -0700

    last saw

--- mods/tell.py
+++ mods/tell.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 import isla
+from util.timedelta import td_format
 
 @isla.bind("hear", ".*", i=True)
 def monitor_channel(self, c, e, msg, match):
@@ -8,6 +11,7 @@ def monitor_channel(self, c, e, msg, match):
         for source, message in self.tells[nick].iteritems():
             self.reply(c,e,"{name} told me to tell you: \"{message}\"".format(name=source,message=message))
         del self.tells[nick]
+    self.lastsaw[nick] = datetime.datetime.now()
 
 @isla.bind("reply", "^tell +([-a-zA-Z0-9]*):? +(.*)$", i=True)
 def receive_tell(self, c, e, msg, match):
@@ -18,5 +22,18 @@ def receive_tell(self, c, e, msg, match):
     self.tells[target][e.source.nick] = message
     self.reply(c,e,"I'll let them know when they next speak up.")
 
+@isla.bind("reply", "^when did you last see ([-a-zA-Z0-9]*)\??$", i=True)
+def last_saw(self, c, e, msg, match):
+    target = match.group(1)
+    if not target.lower() in self.lastsaw:
+        self.reply(c,e,"I've not seen {person}.".format(person=target))
+    else:
+        now = datetime.datetime.now()
+        delta = now - self.lastsaw[target.lower()]
+        time = td_format(delta)
+        self.reply(c,e,"I last saw {person} {time} ago".format(person=target,time=time))
+
 if not 'tells' in dir(isla.bot.isla):
     isla.bot.isla.tells = {}
+if not 'lastsaw' in dir(isla.bot.isla):
+    isla.bot.isla.lastsaw = {}
---
+++ util/timedelta.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+def td_format(td_object):
+    seconds = int(td_object.total_seconds())
+    periods = [
+        ('year',        60*60*24*365),
+        ('month',       60*60*24*30),
+        ('day',         60*60*24),
+        ('hour',        60*60),
+        ('minute',      60),
+        ('second',      1)
+    ]
+    strings=[]
+    for period_name,period_seconds in periods:
+        if seconds > period_seconds:
+            period_value , seconds = divmod(seconds,period_seconds)
+            if period_value == 1:
+                strings.append("%s %s" % (period_value, period_name))
+            else:
+                strings.append("%s %ss" % (period_value, period_name))
+    return ", ".join(strings)
+
