21891f1e13e5b3a219185e4fef128b7ba7938b23
Kevin Lange <klange@dakko.us>
2015-04-27 23:24:29 -0700

    Good morning

--- mods/hello.py
+++ mods/hello.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 import isla
 
 @isla.bind("reply", "^hello[.!]?$", i=True)
 def reply_hello(self, c, e, msg, match):
     self.reply(c,e,"Hello!")
+
+@isla.bind("reply", "^(good )?morning[.!]?$", i=True)
+def good_morning(self, c, e, msg, match):
+    h = datetime.datetime.now().hour
+    if h < 4:
+        self.reply(c,e,"It's too early...")
+    elif h > 12:
+        self.reply(c,e,"It's too late...")
+    else:
+        self.reply(c,e,"Good morning.")
 
